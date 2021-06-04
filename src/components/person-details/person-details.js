96f38dfdc46072d1021028a7bebdaf329fd0439d
Nikolay <harnik1988@gmail.com>
2021-06-04 06:20:25 +0300

    loading char details

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -9,7 +9,8 @@ export default class PersonDetails extends Component {
   swapiService = new SwapiService();
 
   state = {
-    person: null
+    person: null,
+    loading: false
   }
 
   componentDidMount = () => {
@@ -19,6 +20,9 @@ export default class PersonDetails extends Component {
   componentDidUpdate = (prevProps) => {
     if (this.props.idSelected !== prevProps.idSelected) {
       this.updatePerson();
+      this.setState({
+        loading: true
+      })
     }
   }
 
@@ -30,7 +34,10 @@ export default class PersonDetails extends Component {
 
     this.swapiService.getPerson(idSelected)
       .then((person) => {
-        this.setState({ person })
+        this.setState({
+          person,
+          loading: false
+        })
       })
 
 
@@ -41,30 +48,42 @@ export default class PersonDetails extends Component {
     if (!this.state.person) {
       return <h3>Выберите персонажа...</h3>
     }
-    const { person: { id, name, gender, birthYear, eyeColor } } = this.state;
+    const { person, loading } = this.state;
+    const content = loading ? <Spinner /> : <Person person={person} />;
 
     return (
       <div className="person-details card">
-        <img className="person-image"
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
-        <div className="card-body">
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>{eyeColor}</span>
-            </li>
-          </ul>
-        </div>
+        {content}
       </div>
     )
   }
+}
+
+const Person = ({ person }) => {
+  const { id, name, gender, birthYear, eyeColor } = person;
+  return (
+    <>
+      <img className="person-image"
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+      <div className="card-body">
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Gender</span>
+            <span>{gender}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Birth Year</span>
+            <span>{birthYear}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Eye Color</span>
+            <span>{eyeColor}</span>
+          </li>
+        </ul>
+      </div>
+
+    </>
+
+  )
 }
