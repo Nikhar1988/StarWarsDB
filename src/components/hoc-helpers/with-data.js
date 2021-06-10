3a1ecd52cb2d0529782a1180afdea7714b241e6a
Nikolay <harnik1988@gmail.com>
2021-06-10 16:58:01 +0300

    refactoring1

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import ErrorIndicator from '../error-indicator';
 import Spinner from '../spinner';
 
 
@@ -7,23 +7,52 @@ const withData = (View) => {
   return class extends Component {
 
     state = {
-      data: null
+      data: null,
+      loading: true,
+      error: false
     };
 
-    componentDidMount() {
+    
+    componentDidUpdate =(prefprops) => {
+      if(this.props.getData !== prefprops.getData) {
+        this.update();
+      }
+
+
+    }
+    update=() => {
+      this.setState({
+        loading: true,
+        error: false
+      })
       this.props.getData()
         .then((data) => {
           this.setState({
-            data
+            data,
+            loading: false
           });
-        });
+        })
+        .catch(() =>{
+          this.setState({
+            loading: false,
+            error: true
+          })
+        })
+    }
+    
+    componentDidMount = () => {
+      this.update();
     }
 
     render() {
-      const { data } = this.state;
+      const { data, loading, error } = this.state;
 
-      if (!data) {
+      if (loading) {
         return <Spinner />;
+      }
+
+      if (error) {
+        return <ErrorIndicator/>;
       }
 
       return <View {...this.props} data={data} />;
---
+++ src/components/pages/index.js
@@ -0,0 +1,11 @@
+import PeoplePage from './people-page';
+import PlanetPage from './planet-page';
+import StarshipPage from './starship-page';
+
+
+
+export {
+    PeoplePage,
+    StarshipPage,
+    PlanetPage
+};
